90d848f1fa432dd8fb9e57f3da3dbd85f959ec7d
leahzeisner <leahzeisner@gmail.com>
2024-02-18 21:51:49 -0500

    course component

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { FaEllipsisV } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import { courses } from "../Database";
+import Course from "./Course";
 import "./index.css";
 
 function Dashboard() {
@@ -20,27 +20,7 @@ function Dashboard() {
 
         <div className="d-flex flex-wrap">
           {courses.map((course) => (
-            <div key={course._id} className="card">
-              <img src={`/images/${course.image}`} className="card-img-top" />
-
-              <div className="card-body">
-                <Link
-                  className="card-title"
-                  to={`/Kanbas/Courses/${course._id}/Home`}
-                >
-                  {course.name}
-                </Link>
-
-                <p className="card-text card-section">{course.number}</p>
-                <p className="card-text card-term">{course.term}</p>
-                <Link
-                  to={`/Kanbas/Courses/${course._id}/Home`}
-                  className="btn btn-primary go"
-                >
-                  Go{" "}
-                </Link>
-              </div>
-            </div>
+            <Course course={course} />
           ))}
         </div>
       </div>
