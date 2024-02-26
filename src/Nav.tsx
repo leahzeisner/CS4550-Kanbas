eaecca22c40fbd4bc950c2164d8a52f08321c847
leahzeisner <leahzeisner@gmail.com>
2024-02-26 17:35:26 -0500

    fix lab/lanbas nav

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,28 +1,44 @@
 import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+
 function Nav() {
   const { pathname } = useLocation();
 
   return (
-    <nav className="nav nav-tabs mt-2">
-      <Link
-        to="/Labs/a3"
-        className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}
-      >
-        A3
-      </Link>
-      <Link
-        to="/Kanbas"
-        className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}
-      >
-        Kanbas
-      </Link>
-      <Link
-        to="/hello"
-        className={`nav-link ${pathname.includes("hello") ? "active" : ""}`}
-      >
-        Hello
-      </Link>
-    </nav>
+    <ul className="nav nav-tabs mt-2">
+      <li className="nav-item">
+        <Link
+          to="/Labs/a3"
+          className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}
+        >
+          A3
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Labs/a4"
+          className={`nav-link ${pathname.includes("a4") ? "active" : ""}`}
+        >
+          A4
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/hello"
+          className={`nav-link ${pathname.includes("hello") ? "active" : ""}`}
+        >
+          Hello
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Kanbas"
+          className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}
+        >
+          Kanbas
+        </Link>
+      </li>
+    </ul>
   );
 }
 export default Nav;
