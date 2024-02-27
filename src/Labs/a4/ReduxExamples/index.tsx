ed1c969fd9b817567f0daecc34d9635aec3a012c
leahzeisner <leahzeisner@gmail.com>
2024-02-26 20:03:41 -0500

    a4 lab through 2.4.4

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import AddRedux from "./AddRedux";
+import CounterRedux from "./CounterRedux";
+import HelloRedux from "./HelloRedux";
 
 const ReduxExamples = () => {
   return (
     <div>
       <h2>Redux Examples</h2>
+
+      <HelloRedux />
+      <CounterRedux />
+      <AddRedux />
     </div>
   );
 };
