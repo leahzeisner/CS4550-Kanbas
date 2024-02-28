64c04b8faafca5edb9c36430869b8053af98ddcb
leahzeisner <leahzeisner@gmail.com>
2024-02-27 21:05:26 -0500

    style add/edit course

--- src/Kanbas/Dashboard/AddCourse.tsx
+++ src/Kanbas/Dashboard/AddCourse.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { emptyCourse, validateForm } from "./constants";
 import { Course } from "../types";
+import { FaPlus, FaX } from "react-icons/fa6";
 
 function AddCourse({ addCourse }: { addCourse: (course: Course) => void }) {
   const [course, setCourse] = useState<Course>(emptyCourse);
   const [addCourseEnabled, setAddCourseEnabled] = useState(true);
+  const [addingCourse, setAddingCourse] = useState(false);
 
   useEffect(() => {
     setAddCourseEnabled(validateForm(course));
@@ -19,55 +21,83 @@ function AddCourse({ addCourse }: { addCourse: (course: Course) => void }) {
       };
       setCourse(emptyCourse);
       addCourse(newCourse);
+      setAddingCourse(false);
     }
   };
 
   return (
     <div className="add-edit-courses">
-      <h5>Add Course</h5>
-      <input
-        value={course.name}
-        placeholder="Course Name"
-        className="form-control add-edit-courses-input"
-        type="text"
-        onChange={(e) => setCourse({ ...course, name: e.target.value })}
-      />
-      <input
-        value={course.number}
-        placeholder="Course Number"
-        className="form-control add-edit-courses-input"
-        type="text"
-        onChange={(e) => setCourse({ ...course, number: e.target.value })}
-      />
-      <input
-        value={course.term}
-        placeholder="Course Term"
-        className="form-control add-edit-courses-input"
-        type="text"
-        onChange={(e) => setCourse({ ...course, term: e.target.value })}
-      />
-      <input
-        value={course.startDate}
-        placeholder="Course Start Date"
-        className="form-control add-edit-courses-input"
-        type="date"
-        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
-      />
-      <input
-        value={course.endDate}
-        placeholder="Course End Date"
-        className="form-control add-edit-courses-input"
-        type="date"
-        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
-      />
-      <button
-        type="button"
-        id="addEditCourseBtn"
-        onClick={addNewCourse}
-        disabled={!addCourseEnabled}
-      >
-        Add
-      </button>
+      <div className="add-edit-courses-title">
+        <h5>Add Course</h5>
+
+        <button
+          type="button"
+          id="addCoursesArrowBtn"
+          onClick={() => setAddingCourse(!addingCourse)}
+        >
+          {addingCourse ? (
+            <FaX className="ms-2" size={18}></FaX>
+          ) : (
+            <FaPlus className="ms-2" size={20}></FaPlus>
+          )}
+        </button>
+      </div>
+
+      {addingCourse && (
+        <div>
+          <div className="add-edit-courses-container">
+            <input
+              value={course.name}
+              placeholder="Course Name"
+              className="form-control add-edit-courses-input"
+              type="text"
+              onChange={(e) => setCourse({ ...course, name: e.target.value })}
+            />
+            <input
+              value={course.number}
+              placeholder="Course Number"
+              className="form-control add-edit-courses-input"
+              type="text"
+              onChange={(e) => setCourse({ ...course, number: e.target.value })}
+            />
+            <input
+              value={course.term}
+              placeholder="Course Term"
+              className="form-control add-edit-courses-input"
+              type="text"
+              onChange={(e) => setCourse({ ...course, term: e.target.value })}
+            />
+
+            <input
+              value={course.startDate}
+              placeholder="Course Start Date"
+              className="form-control add-edit-courses-input"
+              type="date"
+              onChange={(e) =>
+                setCourse({ ...course, startDate: e.target.value })
+              }
+            />
+            <input
+              value={course.endDate}
+              placeholder="Course End Date"
+              className="form-control add-edit-courses-input"
+              type="date"
+              onChange={(e) =>
+                setCourse({ ...course, endDate: e.target.value })
+              }
+            />
+          </div>
+
+          <button
+            type="button"
+            id="addEditCourseBtn"
+            onClick={addNewCourse}
+            disabled={!addCourseEnabled}
+          >
+            Add
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/Kanbas/Dashboard/EditCourse.tsx
+++ src/Kanbas/Dashboard/EditCourse.tsx
@@ -32,62 +32,84 @@ function EditCourse({
     }
   };
 
+  const onCancelEdit = () => {
+    setEditableCourse(emptyCourse);
+    setIsAdding(true);
+  };
+
   return (
     <div className="add-edit-courses">
       <h5>Edit Course</h5>
-      <input
-        value={editableCourse.name}
-        placeholder="Course Name"
-        className="form-control add-edit-courses-input"
-        type="text"
-        onChange={(e) =>
-          setEditableCourse({ ...editableCourse, name: e.target.value })
-        }
-      />
-      <input
-        value={editableCourse.number}
-        placeholder="Course Number"
-        className="form-control add-edit-courses-input"
-        type="text"
-        onChange={(e) =>
-          setEditableCourse({ ...editableCourse, number: e.target.value })
-        }
-      />
-      <input
-        value={editableCourse.term}
-        placeholder="Course Term"
-        className="form-control add-edit-courses-input"
-        type="text"
-        onChange={(e) =>
-          setEditableCourse({ ...editableCourse, term: e.target.value })
-        }
-      />
-      <input
-        value={editableCourse.startDate}
-        placeholder="Course Start Date"
-        className="form-control add-edit-courses-input"
-        type="date"
-        onChange={(e) =>
-          setEditableCourse({ ...editableCourse, startDate: e.target.value })
-        }
-      />
-      <input
-        value={editableCourse.endDate}
-        placeholder="Course End Date"
-        className="form-control add-edit-courses-input"
-        type="date"
-        onChange={(e) =>
-          setEditableCourse({ ...editableCourse, endDate: e.target.value })
-        }
-      />
-      <button
-        type="button"
-        id="addEditCourseBtn"
-        onClick={updateCourse}
-        disabled={!updateCourseEnabled}
-      >
-        Update
-      </button>
+
+      <div className="add-edit-courses-container">
+        <input
+          value={editableCourse.name}
+          placeholder="Course Name"
+          className="form-control add-edit-courses-input"
+          type="text"
+          onChange={(e) =>
+            setEditableCourse({ ...editableCourse, name: e.target.value })
+          }
+        />
+        <input
+          value={editableCourse.number}
+          placeholder="Course Number"
+          className="form-control add-edit-courses-input"
+          type="text"
+          onChange={(e) =>
+            setEditableCourse({ ...editableCourse, number: e.target.value })
+          }
+        />
+        <input
+          value={editableCourse.term}
+          placeholder="Course Term"
+          className="form-control add-edit-courses-input"
+          type="text"
+          onChange={(e) =>
+            setEditableCourse({ ...editableCourse, term: e.target.value })
+          }
+        />
+        <input
+          value={editableCourse.startDate}
+          placeholder="Course Start Date"
+          className="form-control add-edit-courses-input"
+          type="date"
+          onChange={(e) =>
+            setEditableCourse({
+              ...editableCourse,
+              startDate: e.target.value,
+            })
+          }
+        />
+        <input
+          value={editableCourse.endDate}
+          placeholder="Course End Date"
+          className="form-control add-edit-courses-input"
+          type="date"
+          onChange={(e) =>
+            setEditableCourse({ ...editableCourse, endDate: e.target.value })
+          }
+        />
+      </div>
+
+      <div className="edit-course-ftr-btns">
+        <button
+          type="button"
+          id="addEditCourseBtn"
+          className="edit-course-cancel-btn"
+          onClick={onCancelEdit}
+        >
+          Cancel
+        </button>
+        <button
+          type="button"
+          id="addEditCourseBtn"
+          onClick={updateCourse}
+          disabled={!updateCourseEnabled}
+        >
+          Update
+        </button>
+      </div>
     </div>
   );
 }
