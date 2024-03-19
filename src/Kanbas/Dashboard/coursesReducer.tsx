15c16cede08e6a0cbb35453e579aed6203f2f108
leahzeisner <leahzeisner@gmail.com>
2024-03-19 16:41:52 -0400

    add back setCourse reducer action

--- src/Kanbas/Dashboard/coursesReducer.tsx
+++ src/Kanbas/Dashboard/coursesReducer.tsx
@@ -9,11 +9,14 @@ const coursesSlice = createSlice({
   name: "coursesReducer",
   initialState,
   reducers: {
+    setCourse: (state, action) => {
+      state.course = action.payload;
+    },
     setPage: (state, action) => {
       state.page = action.payload;
     },
   },
 });
 
-export const { setPage } = coursesSlice.actions;
+export const { setCourse, setPage } = coursesSlice.actions;
 export default coursesSlice.reducer;
