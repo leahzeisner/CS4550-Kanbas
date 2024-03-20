5456475ccaf0c4e9a2b6d5305b65727b7a6bf404
leahzeisner <leahzeisner@gmail.com>
2024-03-19 20:19:14 -0400

    delete database

--- src/Kanbas/Database/index.tsx
+++
@@ -1,7 +0,0 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import todos from "./todos.json";
-import comingUpItems from "./coming-up.json";
-import assignments from "./assignments.json";
-
-export { courses, modules, todos, comingUpItems, assignments };
