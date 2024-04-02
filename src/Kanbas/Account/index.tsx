c9545e3d63aed79ed877df5ebcfc6f0a79eeb8d8
leahzeisner <leahzeisner@gmail.com>
2024-04-02 09:46:41 -0400

    if user logged in, navigate to profile otherwise signin

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,13 +1,31 @@
 import Signin from "../Users/Signin";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Profile from "../Users/Profile";
 import UserTable from "../Users/Table";
 import Signup from "../Users/Signup";
+import { useEffect } from "react";
+import * as client from "../Users/client";
+
 export default function Account() {
+  const navigate = useNavigate();
+
+  const getCurrentUser = async () => {
+    try {
+      await client.profile();
+      navigate("/Kanbas/Account/Profile");
+    } catch (err: any) {
+      navigate("/Kanbas/Account/Signin");
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+        <Route path="/" />
         <Route path="/Signin" element={<Signin />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/Profile" element={<Profile />} />
