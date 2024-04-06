fbadc51a250b306990a1089d307c77e8a92c5a25
leahzeisner <leahzeisner@gmail.com>
2024-04-05 20:42:11 -0400

    make quiz title a link, install mui, add quiz context menu

--- src/Kanbas/Courses/Quizzes/Quiz.tsx
+++ src/Kanbas/Courses/Quizzes/Quiz.tsx
@@ -1,9 +1,19 @@
 import { FaCheckCircle, FaEllipsisV } from "react-icons/fa";
 import { FaBan, FaRocket } from "react-icons/fa6";
 import { useDispatch } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { Quiz as QuizType } from "../../types";
 import { formatDateTime } from "../../utils";
-import { updateQuiz } from "./quizzesReducer";
+import { deleteQuiz, updateQuiz } from "./quizzesReducer";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import {
+  usePopupState,
+  bindTrigger,
+  bindMenu,
+} from "material-ui-popup-state/hooks";
 
 interface QuizProps {
   quiz: QuizType;
@@ -13,6 +23,9 @@ const getSpacer = () => <span>&nbsp; | &nbsp;</span>;
 
 function Quiz({ quiz }: QuizProps) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { courseId } = useParams();
+  const popupState = usePopupState({ variant: "popover", popupId: "demoMenu" });
 
   const getAvailability = () => {
     const currentDate = new Date();
@@ -42,13 +55,20 @@ function Quiz({ quiz }: QuizProps) {
   const getQuizInfo = () => {
     return (
       <>
+        {/* Availability */}
         {getAvailability()}
+        {/* Due Date */}
         <span className="quiz-info-bold">Due </span>
         <span>{formatDateTime(quiz.dueDate)}</span>
-        {getSpacer()}
-        <span>{quiz.points || "0"} pts</span>
-        {getSpacer()}
-        <span>{quiz.questions.length} Questions</span>
+        {/* Points and Questions if published */}
+        {quiz.published && (
+          <>
+            {getSpacer()}
+            <span>{quiz.points || "0"} pts</span>
+            {getSpacer()}
+            <span>{quiz.questions.length} Questions</span>
+          </>
+        )}
       </>
     );
   };
@@ -57,8 +77,13 @@ function Quiz({ quiz }: QuizProps) {
     dispatch(updateQuiz({ ...quiz, published: !quiz.published }));
   };
 
-  const onQuizContextToggle = () => {
-    // toggle quiz context menu
+  const getQuizDetailsPath = () => {
+    return `/Kanbas/Courses/${courseId}/Quizzes/${quiz._id}`;
+  };
+
+  const onDeleteQuiz = () => {
+    dispatch(deleteQuiz(quiz));
+    popupState.close();
   };
 
   return (
@@ -75,7 +100,11 @@ function Quiz({ quiz }: QuizProps) {
       </button>
 
       <div className="quiz-container quizzes-text-container">
-        <div className="quiz-title">{quiz.title}</div>
+        <div className="quiz-title">
+          <Link id="quiz-title-link" to={getQuizDetailsPath()}>
+            {quiz.title}
+          </Link>
+        </div>
         <div className="quiz-info">{getQuizInfo()}</div>
       </div>
 
@@ -87,13 +116,19 @@ function Quiz({ quiz }: QuizProps) {
             <FaBan className="unpublish-quiz-btns" />
           )}
         </button>
-        <button
-          type="button"
-          className="quiz-btn"
-          onClick={onQuizContextToggle}
-        >
+        <Button type="button" variant="contained" {...bindTrigger(popupState)}>
           <FaEllipsisV className="ms-2" />
-        </button>
+        </Button>
+        <Menu {...bindMenu(popupState)}>
+          <MenuItem onClick={() => navigate(getQuizDetailsPath())}>
+            Edit
+          </MenuItem>
+          <MenuItem onClick={onDeleteQuiz}>Delete</MenuItem>
+          <MenuItem onClick={onPublishToggle}>Publish</MenuItem>
+          {/* Optional */}
+          {/* <MenuItem onClick={popupState.close}>Copy</MenuItem>
+          <MenuItem onClick={popupState.close}>Sort</MenuItem> */}
+        </Menu>
       </div>
     </li>
   );
